e6ef7533d753b04d65ecb78cb17e1030d6825408
Cam Soper <cam.soper@microsoft.com>
2022-10-05 15:04:59 -0500

    Rename method

--- tutorials/InputTutorial/Program.cs
+++ tutorials/InputTutorial/Program.cs
@@ -10,11 +10,11 @@ controller.OpenPin(pin, PinMode.InputPullUp);
 Console.WriteLine($"Initial status ({DateTime.Now}): {(controller.Read(pin) == PinValue.High ? alert : ready)}");
 controller.RegisterCallbackForPinValueChangedEvent(pin, 
     PinEventTypes.Falling | PinEventTypes.Rising, 
-    (sender, args) => onButtonChange(sender, args));
+    (sender, args) => onPinEvent(sender, args));
 
 Thread.Sleep(Timeout.Infinite);
 
-void onButtonChange(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
+void onPinEvent(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
 {     
     Console.WriteLine($"({DateTime.Now}) {(pinValueChangedEventArgs.ChangeType == PinEventTypes.Rising ? alert : ready)}");
 }
