82de8ea2418f16ef0f27e0ff4cedafd6a136c1af
Cam Soper <cam.soper@microsoft.com>
2024-01-23 16:27:21 -0600

    adding ft232h list sample

---
+++ tutorials/ft232h/ft232h.list/Program.cs
@@ -0,0 +1,19 @@
+﻿using Iot.Device.FtCommon;
+
+var devices = FtCommon.GetDevices();
+Console.WriteLine($"{devices.Count} available device(s)");
+foreach (var device in devices)
+{
+    Console.WriteLine($"  {device.Description}");
+    Console.WriteLine($"    Flags: {device.Flags}");
+    Console.WriteLine($"    Id: {device.Id}");
+    Console.WriteLine($"    LocId: {device.LocId}");
+    Console.WriteLine($"    Serial number: {device.SerialNumber}");
+    Console.WriteLine($"    Type: {device.Type}");
+}
+
+if (devices.Count == 0)
+{
+    Console.WriteLine("No device connected");
+    return;
+}
